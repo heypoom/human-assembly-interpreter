ec73aa8ebc4fd8d0214544b7ec85a213bf909d1a
Phoomparin Mano <poom@hey.com>
2021-11-04 01:28:53 +0700

    feat: exception handling

--- src/main/kotlin/dev/poom/utils/InstructionParser.kt
+++ src/main/kotlin/dev/poom/utils/InstructionParser.kt
@@ -2,11 +2,14 @@ package dev.poom.utils
 
 data class Op(val instruction: Instruction, val values: List<*>)
 
-inline fun <reified T : Enum<T>> intoEnum(name: String): T? {
-    return try {
-        enumValueOf<T>(name.uppercase())
-    } catch (e: IllegalArgumentException) { null }
-}
+inline fun <reified T> nullify(fn: () -> T): T? =
+    try { fn() } catch (e: Exception) { null }
+
+inline fun <reified T : Enum<T>> intoEnum(name: String): T? =
+    nullify { enumValueOf<T>(name.uppercase()) }
+
+fun intoNumeric(value: String): Int? =
+    nullify { value.toInt() }
 
 object InstructionParser {
     fun parse(code: String): List<Op> {
@@ -25,7 +28,7 @@ object InstructionParser {
         return list
     }
 
-    private fun valuesOf(strings: List<String>) = strings.map { registerOf(it) ?: it.toInt() }
+    private fun valuesOf(strings: List<String>) = strings.map { registerOf(it) ?: intoNumeric(it) }
     private fun instructionOf(name: String) = intoEnum<Instruction>(name)
     private fun registerOf(name: String) = intoEnum<Register>(name)
 }
