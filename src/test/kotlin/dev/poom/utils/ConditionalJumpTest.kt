6809f35e88e1b60768217c04b3e5a0ccf736b377
Phoomparin Mano <poom@hey.com>
2021-11-05 00:00:38 +0700

    test: assert conditional jump

--- src/test/kotlin/dev/poom/utils/ConditionalJumpTest.kt
+++ src/test/kotlin/dev/poom/utils/ConditionalJumpTest.kt
@@ -10,12 +10,18 @@ internal class ConditionalJumpTest {
         o.run("""
             mov eax, 1
             inc eax
-            cmp eax, 10
+            cmp eax, 5
             jb 1
-            add eax, 59
+            mov ebx, 69
         """)
 
-        println(o.m.state)
-        assertEquals(69, o.m.value(Register.EAX))
+        // Expect loop to increment EAX five times
+        assertEquals(5, o.m.value(Register.EAX))
+
+        // Expect last line of instruction to be run
+        assertEquals(69, o.m.value(Register.EBX))
+
+        // Expect the zero flag from the last comparison to be set (
+        assertEquals(true, o.m.flags[Flag.ZERO])
     }
 }
